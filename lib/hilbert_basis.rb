48b63ada7952eb96d6628942778ac157b02a8a06
Jeff Remer <jeff@threestarchina.com>
2011-03-21 14:19:12 -0700

    Changed path to normalize and added temp to ingore

--- lib/hilbert_basis.rb
+++ lib/hilbert_basis.rb
@@ -1,6 +1,6 @@
 require 'matrix'
 class HilbertBasis
-  NORMALIZ_COMMAND = "/Applications/Normaliz/norm64 -f %s"
+  NORMALIZ_COMMAND = "norm64 -f %s"
   INPUT_FILENAME = "temp/tempfile.in"
   OUTPUT_FILENAME = "temp/tempfile.gen"
   
